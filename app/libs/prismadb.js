4f441778c1422be3ec0b8306e9a10b5ddc2b5980
GabrielSiwa <gabrielirasiwa.8@gmail.com>
2025-03-30 17:00:48 -0600

    PrismaDB  & yarn

--- app/libs/prismadb.js
+++ app/libs/prismadb.js
@@ -31,36 +31,3 @@ const prisma = globalForPrisma.prisma || prismaClientSingleton();
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
-
-// import { PrismaClient } from "@prisma/client";
-
-// // Check if we're in a build/static environment
-// const isBuildTime =
-//   process.env.NODE_ENV === "production" &&
-//   process.env.NEXT_PHASE === "phase-production-build";
-
-// // Create a mock PrismaClient for build time
-// const MockPrismaClient = {
-//   movie: {
-//     findMany: async () => [],
-//     findUnique: async () => null,
-//     create: async (data) => data.data,
-//     update: async (data) => data.data,
-//     delete: async () => ({}),
-//   },
-//   $connect: async () => {},
-//   $disconnect: async () => {},
-// };
-
-// // Use the mock client during build, real client otherwise
-// const prismaClientSingleton = () => {
-//   return isBuildTime ? MockPrismaClient : new PrismaClient();
-// };
-
-// const globalForPrisma = globalThis;
-
-// const prisma = globalForPrisma.prisma || prismaClientSingleton();
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-// export default prisma;
